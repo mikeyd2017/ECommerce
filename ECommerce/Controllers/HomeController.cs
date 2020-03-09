bf5dc3f05af4d9ed2e5c47c631a21bfad62c4710
Michael D'Ambrosio <dambrosiomichael@rocketmail.com>
2020-03-09 12:36:23 -0500

    BigMaad

--- ECommerce/Controllers/HomeController.cs
+++ ECommerce/Controllers/HomeController.cs
@@ -34,6 +34,11 @@ namespace ECommerce.Controllers
             return View();
         }
 
+        public IActionResult bigMad()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
