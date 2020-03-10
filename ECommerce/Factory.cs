531b3720941ed58024679f3ca2b620fefec83764
Michael D'Ambrosio <dambrosiomichael@rocketmail.com>
2020-03-09 20:55:15 -0500

    Update future Factory comments for sessions/cookies

--- ECommerce/Factory.cs
+++ ECommerce/Factory.cs
@@ -11,6 +11,8 @@ namespace ECommerce
     public class Factory
     {
         //Only 2 classes needed right now, as mentioned in HomeController we will be adding many more, this will be the best way to keep them all together.
+        //In the future I want to make this so that we set certain session or cookie values for the Factory, so we only need to load the classes specific to the session or cookie.
+        //That way we don't have to remake the whole factory every time the controller is called, it's not too intensive either way.
         public DataHelper DataHelper;
         public TableCommands TableCommands;
 
