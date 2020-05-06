f276f84d7c3f2a51d7ca2faa388c6c3719a9cadb
Matt <mwissel2018@gmail.com>
2020-05-05 20:15:53 -0500

    Added a department controller and a department helper

---
+++ ECommerce/Controllers/DepartmentController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ECommerce;
+using ECommerce.Helpers;
+using ECommerce.DataModels;
+using ECommerce.SqlCommands;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace FullStackFullTime.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private Factory _Factory = new Factory();
+
+        public DepartmentController(IConfiguration iConfig)
+        {
+            _Factory.DataHelper = new DataHelper(iConfig);
+            _Factory.AccountCommands = new DepartmentCommands(_Factory.DataHelper);
+            _Factory.AccountHelper = new DepartmentHelper(_Factory);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+
+        public IActionResult CreateDept()
+        {
+            Department newDepartment = new Department();
+            
+
+            return View(newDepartment);
+        }
+
+        [HttpPost]
+        public IActionResult CreateDept(string departmentID, string name, string dateCreated)
+        {
+            string errorMessage = _Factory.DepartmentHelper.CheckCreate(departmentID, name, dateCreated);
+            if (!String.IsNullOrEmpty(errorMessage))
+            {
+                Department createDepartment = new Department();
+                createDepartment.DepartmentError = errorMessage;
+
+                return View(createDepartment);
+            }
+
+        }
+
+
+
+
+
+    }
+}
---
+++ ECommerce/Helpers/DepartmentHelper.cs
@@ -0,0 +1,85 @@
+﻿using ECommerce.DataModels;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace ECommerce.Helpers
+{
+    public class DepartmentHelper
+    {
+        private Factory _Factory;
+
+        public const string noDepartmentIDOrName = "Please enter a Department ID and a Department name";
+        public const string noDepartmentIDOrNameOrDateCreated = "Please enter a Department ID, Department Name, and Date Created";
+        public const string noDepartmentID = "Please enter a Department ID";
+        public const string noName = "Please enter a Department name";
+        public const string noDateCreated = "Please enter a date created";
+        public const string departmentNameTaken = "Department Name taken";
+        public const string departmentIDTaken = "Department ID taken";
+        public DepartmentHelper(Factory oFactory)
+        {
+            _Factory = oFactory;
+        }
+       
+
+        public bool CreateDepartment(int departmentID, string departmentName, DateTime dateCreated) 
+        {
+            Department newDepartment = new Department();
+            newDepartment.DepartmentID = departmentID;
+            newDepartment.DepartmentName = departmentName;
+            newDepartment.DateCreated = dateCreated;
+
+            _Factory.DepartmentCommands.InsertDepartment(newDepartment);
+
+            return true;
+        }
+
+    public string CheckCreate(string departmentID, string name, string dateCreated)
+        {
+            string errorMessage = "";
+            List<string> errorMessages = new List<string>();
+
+            if (string.IsNullOrEmpty(departmentID) || string.IsNullOrEmpty(name) || string.IsNullOrEmpty(dateCreated))
+            {
+                errorMessages.Add(noDepartmentIDOrNameOrDateCreated);
+            }
+            else
+            {
+                if (!String.IsNullOrEmpty(_Factory.DepartmentCommands.GetDepartmentID(departmentID)))
+                {
+                    errorMessages.Add(departmentIDTaken);
+                }
+
+                if (!String.IsNullOrEmpty(_Factory.DepartmentCommands.CheckDepartmentName(name)))
+                {
+                    errorMessages.Add(departmentNameTaken);
+                }
+
+            }
+
+            if (errorMessages.Count > 0)
+            {
+                string lastString = errorMessages.Last();
+
+                foreach (string message in errorMessages)
+                {
+                    if (message.Equals(lastString))
+                    {
+                        errorMessage += message;
+                    }
+                    else
+                    {
+                        errorMessage += message + ", ";
+                    }
+                }
+            }
+
+            return errorMessage;
+        }
+
+
+    }
+}
